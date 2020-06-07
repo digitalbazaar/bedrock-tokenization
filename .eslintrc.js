0713a17fdf0a1d99a7d1fdc8cec6ad8529e16172
Dave Longley <dlongley@digitalbazaar.com>
2020-06-07 18:27:24 -0400

    Add core files.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'digitalbazaar'
+  ],
+  ignorePatterns: ['node_modules/']
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# bedrock-tokenization ChangeLog
+
+## 1.0.0 - 2020-xx-xx
+
+### Added
+- Added core files.
+- See git history for changes.
