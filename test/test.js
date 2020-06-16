f5ca7d8539d8d4fee91670ffd555d78a56178e46
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-16 14:49:16 -0400

    Add test files.

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+const bedrock = require('bedrock');
+require('bedrock-https-agent');
+require('bedrock-mongodb');
+
+require('bedrock-test');
+bedrock.start();
+
