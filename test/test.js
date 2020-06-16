5630f00cfabb96fc0bdac4e8910d4f318f7fe098
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-16 15:09:16 -0400

    Rearrange names.

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
 const bedrock = require('bedrock');
-require('bedrock-https-agent');
 require('bedrock-mongodb');
+require('bedrock-tokenization');
+require('bedrock-https-agent');
 
 require('bedrock-test');
 bedrock.start();
-
