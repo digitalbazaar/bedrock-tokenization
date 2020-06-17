171e5e956fd59357e983e23b792378a326439f0f
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-17 11:33:53 -0400

    Finish tests for tokenizer.

--- test/mocha/10-tokenizers.js
+++ test/mocha/10-tokenizers.js
@@ -6,4 +6,27 @@ describe('tokenizers', function() {
     const tokenizer = await tokenizers.getCurrent();
     isTokenizer(tokenizer);
   });
+  it('should get same tokenizer when one is cached', async function() {
+    const tokenizer = await tokenizers.getCurrent();
+    isTokenizer(tokenizer);
+    const cachedTokenizer = await tokenizers.getCurrent();
+    isTokenizer(cachedTokenizer);
+    tokenizer.should.deep.equal(cachedTokenizer);
+  });
+  it('should get tokenizer by id', async function() {
+    const tokenizer = await tokenizers.getCurrent();
+    isTokenizer(tokenizer);
+    const {id} = tokenizer;
+    const databaseTokenizer = await tokenizers.get({id});
+    isTokenizer(databaseTokenizer);
+    tokenizer.id.should.equal(databaseTokenizer.id);
+    const {hmac} = tokenizer;
+    hmac.id.should.equal(databaseTokenizer.hmac.id);
+    hmac.type.should.equal(databaseTokenizer.hmac.type);
+    hmac.algorithm.should.equal(databaseTokenizer.hmac.algorithm);
+    hmac.invocationSigner.id.should.equal(
+      databaseTokenizer.hmac.invocationSigner.id);
+    hmac.kmsClient.keystore.should.equal(
+      databaseTokenizer.hmac.kmsClient.keystore);
+  });
 });
