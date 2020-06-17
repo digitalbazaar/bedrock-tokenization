5b813acbfea3fb56293348c25af26672a8265a8e
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-17 14:47:28 -0400

    Start tests for tokenVersion.

--- test/mocha/10-tokenizers.js
+++ test/mocha/10-tokenizers.js
@@ -1,7 +1,7 @@
 const {tokenizers} = require('bedrock-tokenization');
 const {isTokenizer} = require('./helpers');
 
-describe('tokenizers', function() {
+describe('Tokenizers', function() {
   it('should getCurrent tokenizer when none is cached', async function() {
     const tokenizer = await tokenizers.getCurrent();
     isTokenizer(tokenizer);
