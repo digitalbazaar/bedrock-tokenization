71b4397b54ee62c8a6e8a3b8e748bdcc1d4f2c24
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-17 19:58:58 -0400

    Add more tests.

---
+++ test/mocha/30-tokens.js
@@ -0,0 +1,16 @@
+const {tokens, tokenizers} = require('bedrock-tokenization');
+
+describe('Tokens', function() {
+  let tokenizer = null;
+  before(async function() {
+    tokenizer = await tokenizers.getCurrent();
+  });
+  it('should create a token', async function() {
+    const tokenCount = 5;
+    const attributes = Uint8Array.from(new Set([1, 2]));
+    const internalId = 'foo';
+    const result = await tokens.create(
+      {tokenizer, internalId, attributes, tokenCount});
+    console.log('create tokens', result);
+  });
+});
