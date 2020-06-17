26de8e3b05e2200343bbf1b9b9f54f5330498cf5
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-17 19:58:58 -0400

    Add more tests.

--- test/mocha/40-TokenVersions.js
+++ test/mocha/40-TokenVersions.js
@@ -1,9 +1,10 @@
 const {tokenizers, tokenVersions} = require('bedrock-tokenization');
-const {isTokenVersion} = require('./helpers');
+const {isTokenVersion, cleanDB} = require('./helpers');
 
 describe('TokenVersions', function() {
   let tokenizer = null;
   before(async function() {
+    await cleanDB();
     tokenizer = await tokenizers.getCurrent();
   });
   it('should create a TokenVersion with out an id', async function() {
@@ -17,32 +18,14 @@ describe('TokenVersions', function() {
     await tokenVersions.create({id, options});
   });
   it('should ensureTokenVersion when no existing version', async function() {
-    const tokenizerId = 'test-tokenizer-id';
+    const tokenizerId = 'no-existing-version';
     const result = await tokenVersions.ensureTokenVersion({tokenizerId});
     isTokenVersion(result);
   });
   it('should ensureTokenVersion with an existing version', async function() {
     const {id: tokenizerId} = tokenizer;
-    const expectedOptions = {batchIdSize: 16, batchSaltSize: 99};
     const result = await tokenVersions.ensureTokenVersion({tokenizerId});
-    isTokenVersion(result, expectedOptions);
-  });
-  it('should get TokenVersion by version id', async function() {
-    const {id: tokenizerId} = tokenizer;
-    const expectedOptions = {batchIdSize: 16, batchSaltSize: 99};
-    const tokenVersion = await tokenVersions.ensureTokenVersion({tokenizerId});
-    isTokenVersion(tokenVersion);
-    const {tokenVersion: {id}} = tokenVersion;
-    const result = await tokenVersions.get({id});
-    isTokenVersion(result, expectedOptions);
-    result.tokenVersion.tokenizerId.should.equal(tokenizerId);
-  });
-  it('should get TokenVersion by tokenizerId', async function() {
-    const {id: tokenizerId} = tokenizer;
-    const expectedOptions = {batchIdSize: 16, batchSaltSize: 99};
-    const result = await tokenVersions.get({tokenizerId});
-    isTokenVersion(result, expectedOptions);
-    result.tokenVersion.tokenizerId.should.equal(tokenizerId);
+    isTokenVersion(result);
   });
   it('should set options for TokenVersion', async function() {
     const options = {batchIdSize: 16, batchSaltSize: 99};
@@ -50,6 +33,25 @@ describe('TokenVersions', function() {
     should.exist(result);
     result.should.be.an('boolean');
     result.should.equal(true);
+  });
+  it('should get TokenVersion by version id', async function() {
+    const {id: tokenizerId} = tokenizer;
+    const expectedOptions = {batchIdSize: 16, batchSaltSize: 16};
+    await tokenVersions.setOptions({options: expectedOptions});
+    const tokenVersion = await tokenVersions.ensureTokenVersion({tokenizerId});
+    isTokenVersion(tokenVersion);
+    const {tokenVersion: {id}} = tokenVersion;
+    const result = await tokenVersions.get({id});
+    isTokenVersion(result);
+    result.tokenVersion.tokenizerId.should.equal(tokenizerId);
+  });
+  it('should get TokenVersion by tokenizerId', async function() {
+    const {id: tokenizerId} = tokenizer;
+    const expectedOptions = {batchIdSize: 16, batchSaltSize: 99};
+    await tokenVersions.setOptions({options: expectedOptions});
+    const result = await tokenVersions.get({tokenizerId});
+    isTokenVersion(result);
+    result.tokenVersion.tokenizerId.should.equal(tokenizerId);
   });
   it('should get options', async function() {
     const options = {batchIdSize: 16, batchSaltSize: 99};
