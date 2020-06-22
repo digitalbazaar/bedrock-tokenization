f9ab25ef20de1c540d09f9dc3f27c810ab141cb1
Andrew Jones <ajones@digitalbazaar.com>
2020-06-22 15:52:42 -0400

    Add comments to _createToken.
     Please enter the commit message for your changes. Lines starting

--- test/mocha/30-tokens.js
+++ test/mocha/30-tokens.js
@@ -3,11 +3,19 @@ const {cleanDB} = require('./helpers');
 
 describe('Tokens', function() {
   let tokenizer = null;
-  before(async function() {
+  beforeEach(async function() {
     await cleanDB();
     tokenizer = await tokenizers.getCurrent();
   });
-  it('should create a token', async function() {
+  it('should create a token with attributes', async function() {
+    const tokenCount = 5;
+    const attributes = Uint8Array.from(new Set([1, 2]));
+    const internalId = 'foo';
+    const result = await tokens.create(
+      {tokenizer, internalId, attributes, tokenCount});
+    console.log('create tokens', result);
+  });
+  it('should create a token with out attributes', async function() {
     const tokenCount = 5;
     const attributes = Uint8Array.from(new Set([1, 2]));
     const internalId = 'foo';
