f2c5e24d8f9c703cfd0e529db84c43be519da114
Andrew Jones <ajones@digitalbazaar.com>
2020-06-22 17:52:55 -0400

    Assert on tokens require test module fresh for each test.

--- test/mocha/30-tokens.js
+++ test/mocha/30-tokens.js
@@ -1,25 +1,24 @@
-const {tokens, tokenizers} = require('bedrock-tokenization');
-const {cleanDB} = require('./helpers');
+const {requireUncached, areTokens} = require('./helpers');
+const {tokens, tokenizers} = requireUncached('bedrock-tokenization');
 
 describe('Tokens', function() {
   let tokenizer = null;
   beforeEach(async function() {
-    //await cleanDB();
     tokenizer = await tokenizers.getCurrent();
   });
-  it.skip('should create a token with attributes', async function() {
+  it('should create a token with attributes', async function() {
     const tokenCount = 5;
     const attributes = Uint8Array.from(new Set([1, 2]));
     const internalId = 'foo';
     const result = await tokens.create(
       {tokenizer, internalId, attributes, tokenCount});
-    console.log('create tokens', result);
+    areTokens(result);
   });
   it('should create a token with out attributes', async function() {
     const tokenCount = 5;
     const internalId = 'no-attr';
     const result = await tokens.create(
       {tokenizer, internalId, tokenCount});
-    console.log('create tokens', result);
+    areTokens(result);
   });
 });
