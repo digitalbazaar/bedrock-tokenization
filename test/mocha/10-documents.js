2f5c176eac866404be2d35300c28ba56d79c0330
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-10-30 17:19:18 -0400

    Add unit tests for documents.encrypt().

--- test/mocha/10-documents.js
+++ test/mocha/10-documents.js
@@ -1,5 +1,8 @@
 const {requireUncached, isRegistration} = require('./helpers');
 const {documents} = requireUncached('bedrock-tokenization');
+const {X25519KeyPair} = require('x25519-key-pair');
+const {Cipher} = require('minimal-cipher');
+const cipher = new Cipher();
 
 // this is test data borrowed from minimal-cipher
 const recipients = [
@@ -46,5 +49,65 @@ describe('documents.register()', () => {
 });
 
 describe('documents.encrypt()', () => {
-  it('should encrypt a document with recipients');
+  const key1 = new X25519KeyPair({
+    id: 'did:key:z6MkgDEDniwkugeRADbi5CmHFB2eFdFKh6gSCYHFUeHXaV2x' +
+      '#z6LScXou54NfzNThVwG1aF85TCuFbVPmKuKspufF7eVmHW7G',
+    controller: 'did:key:z6MkgDEDniwkugeRADbi5CmHFB2eFdFKh6gSCYHFUeHXaV2x',
+    type: 'X25519KeyAgreementKey2019',
+    publicKeyBase58: 'rdjYkZotujxQYtF3bc88cgmkLredJ9iwvwZdBrEa8LW',
+    privateKeyBase58: '4HKArAGZaGzwutAEjsbTSjbKDLrQJAP3zLPoZQtHxeuh'
+  });
+
+  const key2 = new X25519KeyPair({
+    id: 'did:key:z6MkrefS4sDAGNBdo7CeXKh52sBfK94NGMANfHKfbYpvPz8S' +
+      '#z6LScKCBLkDApcTvYPbjQi6EDKgpYwWiM9Ppd6X1PbjXF2dg',
+    controller: 'did:key:z6MkrefS4sDAGNBdo7CeXKh52sBfK94NGMANfHKfbYpvPz8S',
+    type: 'X25519KeyAgreementKey2019',
+    publicKeyBase58: 'e21pSQJj9kBT1Dxt4aGtjULhnybeYDfk7oKu95zXerv',
+    privateKeyBase58: 'bcB3uZng7RPz7VSEJSid54cyiU2STGHk4Ub91VEenPP'
+  });
+
+  it('should encrypt a document with recipients', async () => {
+    const recipients = [
+      {header: {kid: key1.id, alg: 'ECDH-ES+A256KW'}},
+      {header: {kid: key2.id, alg: 'ECDH-ES+A256KW'}}
+    ];
+
+    const document = {example: 'document'};
+    const jwe = await documents.encrypt({document, recipients});
+
+    jwe.recipients.should.be.an('array');
+    jwe.recipients.length.should.equal(2);
+
+    const decrypted = await cipher.decryptObject({jwe, keyAgreementKey: key1});
+    decrypted.should.have.property('example', 'document');
+  });
+
+  it('should encrypt a document with a recipientChain', async () => {
+    const recipientChain = [
+      // first pass (inner jwe)
+      [{header: {kid: key1.id, alg: 'ECDH-ES+A256KW'}}],
+      // second pass (outer jwe)
+      [{header: {kid: key2.id, alg: 'ECDH-ES+A256KW'}}]
+    ];
+
+    const document = {example: 'document'};
+    const outerJwe = await documents.encrypt({document, recipientChain});
+
+    outerJwe.recipients.should.be.an('array');
+    outerJwe.recipients.length.should.equal(1);
+
+    const innerJwe = await cipher.decryptObject({
+      jwe: outerJwe, keyAgreementKey: key2
+    });
+
+    innerJwe.recipients.should.be.an('array');
+    innerJwe.recipients.length.should.equal(1);
+
+    const decrypted = await cipher.decryptObject({
+      jwe: innerJwe, keyAgreementKey: key1
+    });
+
+    decrypted.should.have.property('example', 'document');
+  });
 });
