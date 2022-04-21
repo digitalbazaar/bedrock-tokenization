c13f533683c835d2f7731bd243b7ee1a421fbae9
Dave Longley <dlongley@digitalbazaar.com>
2022-04-21 18:29:48 -0400

    Fix linting config.

--- test/.eslintrc.cjs
+++ test/.eslintrc.cjs
@@ -7,6 +7,10 @@ module.exports = {
     node: true,
     mocha: true
   },
+  parserOptions: {
+    // this is required for dynamic import()
+    ecmaVersion: 2020
+  },
   extends: [
     'digitalbazaar'
   ],
