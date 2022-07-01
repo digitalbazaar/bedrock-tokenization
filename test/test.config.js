a4061d467436f2bbe47c0cf5416ab5b06c5dc004
David I. Lehn <dlehn@digitalbazaar.com>
2022-06-30 21:11:34 -0400

    Add IPv6 test address to `ipAllowList`.

--- test/test.config.js
+++ test/test.config.js
@@ -25,4 +25,4 @@ config.mongodb.dropCollections.collections = [];
 config['https-agent'].rejectUnauthorized = false;
 
 config.tokenizer.kms.baseUrl = `${config.server.baseUri}/kms`;
-config.tokenizer.kms.ipAllowList = ['127.0.0.1/32'];
+config.tokenizer.kms.ipAllowList = ['127.0.0.1/32', '::1/128'];
