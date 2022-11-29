c094fc366744dee824d13107fb8e7d05eead20e0
Dave Longley <dlongley@digitalbazaar.com>
2022-11-29 17:41:27 -0500

    Add `registerDocumentAndCreate` unit test.

--- test/mocha/20-tokens.js
+++ test/mocha/20-tokens.js
@@ -121,6 +121,48 @@ describe('Tokens', function() {
       should.exist(tokenResult);
     }
   );
+  it('should create token concurrently with registration',
+    async function() {
+      const dateOfBirth = '1990-05-01';
+      const expires = '2021-05-01';
+      const identifier = 'T65851255';
+      const issuer = 'VA';
+      const type = 'DriversLicense';
+      const recipients = [
+        {
+          header: {
+            kid: 'did:key:z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoA' +
+              'nwWsdvktH#z6LSbysY2xFMRpGMhb7tFTLMpeuPRaqaWM1yECx2AtzE3KCc',
+            alg: 'ECDH-ES+A256KW',
+          }
+        }
+      ];
+      const tokenCount = 1;
+      // canonicalize object then hash it then base58 encode it
+      const externalId = encode(crypto.createHash('sha256')
+        .update(canonicalize({dateOfBirth, identifier, issuer}))
+        .digest());
+
+      let result;
+      let err;
+      try {
+        result = await tokens.registerDocumentAndCreate({
+          registerOptions: {
+            externalId,
+            document: {dateOfBirth, expires, identifier, issuer, type},
+            recipients,
+            ttl: 1209600000
+          },
+          tokenCount
+        });
+      } catch(e) {
+        err = e;
+      }
+      assertNoError(err);
+      should.exist(result);
+      result.should.include.keys(['registrationRecord', 'tokens']);
+    }
+  );
   it('should throw error if attributes is not uint8Array', async function() {
     const tokenCount = 5;
     const internalId = await documents._generateInternalId();
