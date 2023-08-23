1662463d72c12750c6b5f2565ec20de3477d4177
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-08-23 16:22:11 -0400

    Move entities database tests into a separate file.

--- test/mocha/20-tokens.js
+++ test/mocha/20-tokens.js
@@ -5,14 +5,12 @@ import * as database from '@bedrock/mongodb';
 import {areTokens, cleanDB, getTokenBatch, insertRecord} from './helpers.js';
 import {documents, entities, tokens} from '@bedrock/tokenization';
 import {
-  mockEntity1, mockEntity2,
   mockPairwise, mockPairwise2,
   mockTokenBatch, mockTokenBatch2
 } from './mock.data.js';
 import canonicalize from 'canonicalize';
 import crypto from 'node:crypto';
 import {encode} from 'base58-universal';
-import {IdGenerator} from 'bnid';
 import sinon from 'sinon';
 
 const MAX_UINT32 = 4294967295;
@@ -1289,139 +1287,5 @@ describe('Tokens Database Tests', function() {
           'pairwiseToken.internalId': 1, 'pairwiseToken.requester': 1
         });
     });
-  });
-});
-
-describe('Entities Database Tests', function() {
-  describe('Indexes', function() {
-    beforeEach(async () => {
-      const collectionName = 'tokenization-entity';
-      await cleanDB({collectionName});
-
-      const idGenerator = new IdGenerator();
-      mockEntity1.entity.internalId = Buffer.from(
-        await idGenerator.generate());
-      mockEntity1.entity.openBatch[2] = Buffer.from(
-        await idGenerator.generate());
-      mockEntity2.entity.internalId = Buffer.from(
-        await idGenerator.generate());
-
-      // mutliple records are inserted here in order to do proper assertions
-      // for 'nReturned', 'totalKeysExamined' and 'totalDocsExamined'.
-      await insertRecord({
-        record: mockEntity1, collectionName
-      });
-      await insertRecord({
-        record: mockEntity2, collectionName
-      });
-    });
-    it(`is properly indexed for 'entity.internalId' in get()`,
-      async function() {
-        const {internalId} = mockEntity1.entity;
-        const {executionStats} = await entities.get({
-          internalId, explain: true
-        });
-        executionStats.nReturned.should.equal(1);
-        executionStats.totalKeysExamined.should.equal(1);
-        executionStats.totalDocsExamined.should.equal(1);
-        executionStats.executionStages.inputStage.inputStage.inputStage.stage
-          .should.equal('IXSCAN');
-        executionStats.executionStages.inputStage.inputStage.inputStage
-          .keyPattern.should.eql({'entity.internalId': 1});
-      });
-    it(`is properly indexed for 'entity.internalId' in ` +
-      'setMinAssuranceForResolution()', async function() {
-      const {internalId} = mockEntity1.entity;
-      const minAssuranceForResolution = 1;
-      const {executionStats} = await entities.setMinAssuranceForResolution({
-        internalId, minAssuranceForResolution, explain: true
-      });
-      executionStats.nReturned.should.equal(1);
-      executionStats.totalKeysExamined.should.equal(1);
-      executionStats.totalDocsExamined.should.equal(1);
-      executionStats.executionStages.inputStage.inputStage.stage
-        .should.equal('IXSCAN');
-      executionStats.executionStages.inputStage.inputStage.keyPattern
-        .should.eql({'entity.internalId': 1});
-    });
-    it(`is properly indexed for 'entity.internalId' and` +
-      `'entity.batchInvalidationCount' in ` +
-      '_incrementBatchInvalidationCount()', async function() {
-      const {entity} = mockEntity1;
-      const {executionStats} = await entities
-        ._incrementBatchInvalidationCount({entity, explain: true});
-      executionStats.nReturned.should.equal(1);
-      executionStats.totalKeysExamined.should.equal(1);
-      executionStats.totalDocsExamined.should.equal(1);
-      executionStats.executionStages.inputStage.inputStage.stage
-        .should.equal('IXSCAN');
-      executionStats.executionStages.inputStage.inputStage.keyPattern
-        .should.eql({'entity.internalId': 1});
-    });
-    it(`is properly indexed for 'entity.internalId' in _remove()`,
-      async function() {
-        const {internalId} = mockEntity1.entity;
-        const {executionStats} = await entities._remove({
-          internalId, explain: true
-        });
-        executionStats.nReturned.should.equal(1);
-        executionStats.totalKeysExamined.should.equal(1);
-        executionStats.totalDocsExamined.should.equal(1);
-        executionStats.executionStages.inputStage.inputStage.stage
-          .should.equal('IXSCAN');
-        executionStats.executionStages.inputStage.inputStage.keyPattern
-          .should.eql({'entity.internalId': 1});
-      });
-    it(`is properly indexed for 'entity.internalId' and ` +
-      `'entity.batchInvalidationCount' in ` +
-      '_setOpenTokenBatchId()', async function() {
-      const {internalId} = mockEntity1.entity;
-      const batchId = Buffer.from('558fa903-f0b5-4d1c-9d4c-035bfb0d81f9');
-      const batchInvalidationCount = 0;
-      const {executionStats} = await entities._setOpenTokenBatchId({
-        internalId, batchId, batchInvalidationCount, explain: true
-      });
-      executionStats.nReturned.should.equal(1);
-      executionStats.totalKeysExamined.should.equal(1);
-      executionStats.totalDocsExamined.should.equal(1);
-      executionStats.executionStages.inputStage.inputStage.stage
-        .should.equal('IXSCAN');
-      executionStats.executionStages.inputStage.inputStage.keyPattern
-        .should.eql({'entity.internalId': 1});
-    });
-    it(`is properly indexed for 'entity.internalId' and ` +
-      `'entity.openBatch.{minAssuranceForResolution}' in ` +
-      '_setOpenTokenBatchId()', async function() {
-      const {internalId, openBatch} = mockEntity1.entity;
-      const oldBatchId = openBatch[2];
-      const batchId = Buffer.from('558fa903-f0b5-4d1c-9d4c-035bfb0d81f9');
-      const batchInvalidationCount = 0;
-      const {executionStats} = await entities._setOpenTokenBatchId({
-        internalId, batchId, oldBatchId, batchInvalidationCount,
-        minAssuranceForResolution: 2, explain: true
-      });
-      executionStats.nReturned.should.equal(1);
-      executionStats.totalKeysExamined.should.equal(1);
-      executionStats.totalDocsExamined.should.equal(1);
-      executionStats.executionStages.inputStage.inputStage.stage
-        .should.equal('IXSCAN');
-      executionStats.executionStages.inputStage.inputStage.keyPattern
-        .should.eql({'entity.internalId': 1});
-    });
-    it(`is properly indexed for 'entity.internalId' in upsert()`,
-      async function() {
-        const {internalId} = mockEntity1.entity;
-        const ttl = 3000;
-        const {executionStats} = await entities._upsert({
-          internalId, ttl, minAssuranceForResolution: 2, explain: true
-        });
-        executionStats.nReturned.should.equal(1);
-        executionStats.totalKeysExamined.should.equal(1);
-        executionStats.totalDocsExamined.should.equal(1);
-        executionStats.executionStages.inputStage.inputStage.stage
-          .should.equal('IXSCAN');
-        executionStats.executionStages.inputStage.inputStage.keyPattern
-          .should.eql({'entity.internalId': 1});
-      });
   });
 });
---
+++ test/mocha/40-entities.js
@@ -0,0 +1,141 @@
+/*!
+ * Copyright (c) 2020-2023 Digital Bazaar, Inc. All rights reserved.
+ */
+import {cleanDB, insertRecord} from './helpers.js';
+import {mockEntity1, mockEntity2} from './mock.data.js';
+import {entities} from '@bedrock/tokenization';
+import {IdGenerator} from 'bnid';
+
+describe('Entities Database Tests', function() {
+  describe('Indexes', function() {
+    beforeEach(async () => {
+      const collectionName = 'tokenization-entity';
+      await cleanDB({collectionName});
+
+      const idGenerator = new IdGenerator();
+      mockEntity1.entity.internalId = Buffer.from(
+        await idGenerator.generate());
+      mockEntity1.entity.openBatch[2] = Buffer.from(
+        await idGenerator.generate());
+      mockEntity2.entity.internalId = Buffer.from(
+        await idGenerator.generate());
+
+      // mutliple records are inserted here in order to do proper assertions
+      // for 'nReturned', 'totalKeysExamined' and 'totalDocsExamined'.
+      await insertRecord({
+        record: mockEntity1, collectionName
+      });
+      await insertRecord({
+        record: mockEntity2, collectionName
+      });
+    });
+    it(`is properly indexed for 'entity.internalId' in get()`,
+      async function() {
+        const {internalId} = mockEntity1.entity;
+        const {executionStats} = await entities.get({
+          internalId, explain: true
+        });
+        executionStats.nReturned.should.equal(1);
+        executionStats.totalKeysExamined.should.equal(1);
+        executionStats.totalDocsExamined.should.equal(1);
+        executionStats.executionStages.inputStage.inputStage.inputStage.stage
+          .should.equal('IXSCAN');
+        executionStats.executionStages.inputStage.inputStage.inputStage
+          .keyPattern.should.eql({'entity.internalId': 1});
+      });
+    it(`is properly indexed for 'entity.internalId' in ` +
+      'setMinAssuranceForResolution()', async function() {
+      const {internalId} = mockEntity1.entity;
+      const minAssuranceForResolution = 1;
+      const {executionStats} = await entities.setMinAssuranceForResolution({
+        internalId, minAssuranceForResolution, explain: true
+      });
+      executionStats.nReturned.should.equal(1);
+      executionStats.totalKeysExamined.should.equal(1);
+      executionStats.totalDocsExamined.should.equal(1);
+      executionStats.executionStages.inputStage.inputStage.stage
+        .should.equal('IXSCAN');
+      executionStats.executionStages.inputStage.inputStage.keyPattern
+        .should.eql({'entity.internalId': 1});
+    });
+    it(`is properly indexed for 'entity.internalId' and` +
+      `'entity.batchInvalidationCount' in ` +
+      '_incrementBatchInvalidationCount()', async function() {
+      const {entity} = mockEntity1;
+      const {executionStats} = await entities
+        ._incrementBatchInvalidationCount({entity, explain: true});
+      executionStats.nReturned.should.equal(1);
+      executionStats.totalKeysExamined.should.equal(1);
+      executionStats.totalDocsExamined.should.equal(1);
+      executionStats.executionStages.inputStage.inputStage.stage
+        .should.equal('IXSCAN');
+      executionStats.executionStages.inputStage.inputStage.keyPattern
+        .should.eql({'entity.internalId': 1});
+    });
+    it(`is properly indexed for 'entity.internalId' in _remove()`,
+      async function() {
+        const {internalId} = mockEntity1.entity;
+        const {executionStats} = await entities._remove({
+          internalId, explain: true
+        });
+        executionStats.nReturned.should.equal(1);
+        executionStats.totalKeysExamined.should.equal(1);
+        executionStats.totalDocsExamined.should.equal(1);
+        executionStats.executionStages.inputStage.inputStage.stage
+          .should.equal('IXSCAN');
+        executionStats.executionStages.inputStage.inputStage.keyPattern
+          .should.eql({'entity.internalId': 1});
+      });
+    it(`is properly indexed for 'entity.internalId' and ` +
+      `'entity.batchInvalidationCount' in ` +
+      '_setOpenTokenBatchId()', async function() {
+      const {internalId} = mockEntity1.entity;
+      const batchId = Buffer.from('558fa903-f0b5-4d1c-9d4c-035bfb0d81f9');
+      const batchInvalidationCount = 0;
+      const {executionStats} = await entities._setOpenTokenBatchId({
+        internalId, batchId, batchInvalidationCount, explain: true
+      });
+      executionStats.nReturned.should.equal(1);
+      executionStats.totalKeysExamined.should.equal(1);
+      executionStats.totalDocsExamined.should.equal(1);
+      executionStats.executionStages.inputStage.inputStage.stage
+        .should.equal('IXSCAN');
+      executionStats.executionStages.inputStage.inputStage.keyPattern
+        .should.eql({'entity.internalId': 1});
+    });
+    it(`is properly indexed for 'entity.internalId' and ` +
+      `'entity.openBatch.{minAssuranceForResolution}' in ` +
+      '_setOpenTokenBatchId()', async function() {
+      const {internalId, openBatch} = mockEntity1.entity;
+      const oldBatchId = openBatch[2];
+      const batchId = Buffer.from('558fa903-f0b5-4d1c-9d4c-035bfb0d81f9');
+      const batchInvalidationCount = 0;
+      const {executionStats} = await entities._setOpenTokenBatchId({
+        internalId, batchId, oldBatchId, batchInvalidationCount,
+        minAssuranceForResolution: 2, explain: true
+      });
+      executionStats.nReturned.should.equal(1);
+      executionStats.totalKeysExamined.should.equal(1);
+      executionStats.totalDocsExamined.should.equal(1);
+      executionStats.executionStages.inputStage.inputStage.stage
+        .should.equal('IXSCAN');
+      executionStats.executionStages.inputStage.inputStage.keyPattern
+        .should.eql({'entity.internalId': 1});
+    });
+    it(`is properly indexed for 'entity.internalId' in upsert()`,
+      async function() {
+        const {internalId} = mockEntity1.entity;
+        const ttl = 3000;
+        const {executionStats} = await entities._upsert({
+          internalId, ttl, minAssuranceForResolution: 2, explain: true
+        });
+        executionStats.nReturned.should.equal(1);
+        executionStats.totalKeysExamined.should.equal(1);
+        executionStats.totalDocsExamined.should.equal(1);
+        executionStats.executionStages.inputStage.inputStage.stage
+          .should.equal('IXSCAN');
+        executionStats.executionStages.inputStage.inputStage.keyPattern
+          .should.eql({'entity.internalId': 1});
+      });
+  });
+});
